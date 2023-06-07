4e552c5ebb65bc6fa9d1c61ee6ff21217c75ec69
pepesito1 <pepesito1@users.noreply.github.com>
2023-06-07 14:24:32 -0600

    update

---
+++ MHG 採取・発掘確率一覧【森と丘】_files/jwd.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+
+
+}
+/*
+     FILE ARCHIVED ON 17:33:04 Apr 05, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:02:27 Jun 07, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 131.193
+  exclusion.robots: 0.201
+  exclusion.robots.policy: 0.179
+  RedisCDXSource: 1.111
+  esindex: 0.019
+  LoadShardBlock: 38.142 (3)
+  PetaboxLoader3.datanode: 79.694 (5)
+  load_resource: 524.692 (2)
+  PetaboxLoader3.resolve: 474.476 (2)
+*/
---
+++ MHG 採取・発掘確率一覧【火山】_files/jwd.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+
+
+}
+/*
+     FILE ARCHIVED ON 01:46:57 Apr 10, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:01:33 Jun 07, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 181.286
+  exclusion.robots: 0.108
+  exclusion.robots.policy: 0.093
+  RedisCDXSource: 1.422
+  esindex: 0.011
+  LoadShardBlock: 85.052 (3)
+  PetaboxLoader3.datanode: 190.316 (5)
+  load_resource: 518.14 (2)
+  PetaboxLoader3.resolve: 391.139 (2)
+*/
